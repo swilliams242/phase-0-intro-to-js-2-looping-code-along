1cdc34c263de0de1050cb65ab78cb6c67964c2a1
Selena Williams <s.a.williams242@gmail.com>
2023-03-09 11:12:27 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Olli", "Aki"];
+
+function writeCards(cards) {
+    const newCards = [];
+    for (let i = 0; i < cards.length; i++) {
+        newCards.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+
+    return newCards;
+}
+
+writeCards(cards);
+
+
+function countDown(numbers) {
+    let countDown = 10
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
